977c5a3267573884084ea1b079b8792ce17856dc
khaihung2509 <hungprodns1234@gmail.com>
2024-07-30 13:50:04 +0700

    30/7

--- lib/mainLayout.dart
+++ lib/mainLayout.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'CompletedTests.dart';
 import 'Home.dart';
@@ -15,30 +17,91 @@ class mainLayout extends StatefulWidget {
 class _MainLayoutState extends State<mainLayout> {
   int _selectedIndex = 0;
   final _pageController = PageController();
-
+  final _keycontroller = TextEditingController();
   void _onItemTapped(int index) {
     _pageController.jumpToPage(index);
   }
 
+  Future<void> _activateKey(BuildContext context, String key) async {
+    try {
+      // Lấy user ID từ SharedPreferences
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? userId = prefs.getString('idUser');
+
+      if (userId == null) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Không tìm thấy thông tin người dùng.')),
+        );
+        return;
+      }
+
+      // Truy xuất mã key từ Firestore
+      DocumentSnapshot<Map<String, dynamic>> keyDoc = await FirebaseFirestore.instance
+          .collection('Key_Active')
+          .doc(key)
+          .get();
+
+      if (!keyDoc.exists) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Mã key không tồn tại.')),
+        );
+        return;
+      }
+      // Kiểm tra xem mã key đã được sử dụng chưa
+      bool isUsed = keyDoc.data()?['Used'] ?? true;
+      if (isUsed) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Mã key đã được kích hoạt.')),
+        );
+        return;
+      }
+
+      // Lấy thông tin ngày bắt đầu và số ngày hoạt động
+      int daysActive = keyDoc.data()?['Date'] ?? 0;
+      String timeStart = DateFormat('dd-MM-yyyy').format(DateTime.now());
+      String timeEnd = DateFormat('dd-MM-yyyy').format(DateTime.now().add(Duration(days: daysActive)));
+
+      // Cập nhật thông tin mã key trong Firestore
+      await FirebaseFirestore.instance.collection('Key_Active').doc(key).update({
+        'Id_User': userId,
+        'Time_Start': timeStart,
+        'Time_End': timeEnd,
+        'Used': true,
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Kích hoạt mã key thành công.')),
+      );
+      Navigator.of(context).pop();
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Có lỗi xảy ra: $e')),
+      );
+    }
+  }
+
   void _showActivationCodeDialog(BuildContext context) {
+    final TextEditingController _keyController = TextEditingController();
+
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text('Xác nhận mã key'),
           content: SizedBox(
-            width: MediaQuery.of(context).size.width * 0.7, // Chiều rộng 70% màn hình
+            width: MediaQuery.of(context).size.width * 0.7,
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
                 TextField(
                   decoration: InputDecoration(
                     border: OutlineInputBorder(
-                      borderSide: BorderSide(width: 2.0), // Đổi dày viền ở đây
+                      borderSide: BorderSide(width: 2.0),
                     ),
                     hintText: 'Nhập mã key',
-                    contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0), // Giảm chiều cao ở đây
+                    contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
                   ),
+                  controller: _keyController,
                 ),
               ],
             ),
@@ -52,7 +115,14 @@ class _MainLayoutState extends State<mainLayout> {
             ),
             TextButton(
               onPressed: () {
-                // Xử lý mã kích hoạt ở đây
+                String key = _keyController.text.trim();
+                if (key.isNotEmpty) {
+                  _activateKey(context, key);
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Vui lòng nhập mã key.')),
+                  );
+                }
               },
               child: Text('Xác nhận'),
             ),
@@ -66,7 +136,6 @@ class _MainLayoutState extends State<mainLayout> {
     // Xóa dữ liệu trong SharedPreferences
     final prefs = await SharedPreferences.getInstance();
     await prefs.clear();
-
     // Chuyển hướng đến màn hình đăng nhập
     Navigator.pushReplacement(
       context,
