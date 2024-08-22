9cd7f1a2a95b3501d097255ea765e649e1ed5013
Nam <namtran0108642@gmail.com>
2024-08-22 20:44:50 +0700

    endgame

--- lib/Core funtion/TrueFalseQuestion.dart
+++ lib/Core funtion/TrueFalseQuestion.dart
@@ -56,7 +56,7 @@ class _TrueFalseQuestionState extends State<TrueFalseQuestion> {
       final questionId = subQuestions[i]['Id'];
 
       CollectionReference chiTietBoDeRef =
-      FirebaseFirestore.instance.collection('chi_tiet_bo_de');
+          FirebaseFirestore.instance.collection('chi_tiet_bo_de');
 
       try {
         QuerySnapshot snapshot = await chiTietBoDeRef
@@ -67,8 +67,7 @@ class _TrueFalseQuestionState extends State<TrueFalseQuestion> {
         if (snapshot.docs.isNotEmpty) {
           DocumentSnapshot document = snapshot.docs.first;
           String isCorrect = document['IsCorrect'];
-          bool userAnswer; // Lấy đáp án của người dùng
-          userAnswer = isCorrect == 'dung';
+          bool userAnswer = isCorrect == 'dung';
 
           setState(() {
             _answerColors[i] = isCorrect == 'dung' ? Colors.green : Colors.red;
@@ -90,8 +89,9 @@ class _TrueFalseQuestionState extends State<TrueFalseQuestion> {
     bool allCorrect = _selectedAnswers.every((answer) {
       int answerIndex = _selectedAnswers.indexOf(answer);
       return answer != null &&
-          answer == widget.trueFalseQuestion['subQuestions1'][answerIndex]
-          ['correctAnswer'];
+          answer ==
+              widget.trueFalseQuestion['subQuestions1'][answerIndex]
+                  ['correctAnswer'];
     });
 
     widget.onAnswerSelected(allCorrect);
@@ -105,7 +105,7 @@ class _TrueFalseQuestionState extends State<TrueFalseQuestion> {
         final questionId = subQuestions[index]['Id'];
 
         CollectionReference chiTietBoDeRef =
-        FirebaseFirestore.instance.collection('chi_tiet_bo_de');
+            FirebaseFirestore.instance.collection('chi_tiet_bo_de');
 
         try {
           QuerySnapshot snapshot = await chiTietBoDeRef
@@ -116,14 +116,13 @@ class _TrueFalseQuestionState extends State<TrueFalseQuestion> {
             DocumentSnapshot document = snapshot.docs.first;
             await document.reference.update({
               'IsCorrect': _selectedAnswers[index] ==
-                  subQuestions[index]['correctAnswer']
+                      subQuestions[index]['correctAnswer']
                   ? 'dung'
                   : 'sai',
             });
             print('Update successful');
           } else {
-            print(
-                'No document found for the given Id_cau_hoi and Id_bo_de');
+            print('No document found for the given Id_cau_hoi and Id_bo_de');
           }
         } catch (error) {
           print('Failed to update: $error');
@@ -141,86 +140,99 @@ class _TrueFalseQuestionState extends State<TrueFalseQuestion> {
 
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Câu hỏi đúng sai',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
-              textAlign: TextAlign.center,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 50,
+            child: Center(
+              child: Text(
+                'Câu hỏi đúng sai',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 35,
+                    color: Colors.blueAccent),
+              ),
             ),
-            SizedBox(height: 15),
-            for (int i = 0; i < subQuestions.length; i++)
-              Card(
-                elevation: 2,
-                margin: EdgeInsets.symmetric(vertical: 4),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      // Văn bản câu hỏi
-                      Text(
-                        subQuestions[i]['question'],
-                        style: TextStyle(
-                          fontSize: 16, // Giảm kích thước chữ
-                          color: _answerColors[i],
-                        ),
-                      ),
-                      SizedBox(height: 8), // Thêm khoảng cách giữa văn bản câu hỏi và các nút radio
-                      // Các nút radio
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              Text(
-                                "Đúng",
-                                style: TextStyle(fontSize: 14), // Giảm kích thước chữ
-                              ),
-                              Radio<bool?>(
-                                value: true,
-                                groupValue: _selectedAnswers[i],
-                                onChanged: widget.mode == 'lambai'
-                                    ? (value) {
-                                  _onRadioChanged(value, i);
-                                }
-                                    : null,
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Text(
-                                "Sai",
-                                style: TextStyle(fontSize: 14), // Giảm kích thước chữ
-                              ),
-                              Radio<bool?>(
-                                value: false,
-                                groupValue: _selectedAnswers[i],
-                                onChanged: widget.mode == 'lambai'
-                                    ? (value) {
-                                  _onRadioChanged(value, i);
-                                }
-                                    : null,
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ],
+          ),
+          for (int i = 0; i < subQuestions.length; i++)
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 8.0),
+              padding: const EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.grey.shade300),
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.2),
+                    spreadRadius: 3,
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child:
+                  Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                Expanded(
+                  child: Text(
+                    subQuestions[i]['question'],
+                    style: TextStyle(fontSize: 18, color: _answerColors[i]),
+                    softWrap: true,
+                    overflow: TextOverflow.visible,
+                    maxLines: null,
                   ),
                 ),
-              ),
-          ],
-        ),
+                SizedBox(width: 10),
+                // Thêm khoảng cách giữa câu hỏi và các nút radio
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(
+                          height: 20, // Thu nhỏ kích thước nút radio
+                          width: 20,
+                          child: Radio<bool?>(
+                            value: true,
+                            groupValue: _selectedAnswers[i],
+                            onChanged: widget.mode == 'lambai'
+                                ? (value) {
+                                    _onRadioChanged(value, i);
+                                  }
+                                : null,
+                          ),
+                        ),
+                        Text('Đúng'),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 35,
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          height: 20, // Thu nhỏ kích thước nút radio
+                          width: 20,
+                          child: Radio<bool?>(
+                            value: false,
+                            groupValue: _selectedAnswers[i],
+                            onChanged: widget.mode == 'lambai'
+                                ? (value) {
+                                    _onRadioChanged(value, i);
+                                  }
+                                : null,
+                          ),
+                        ),
+                        Text('Sai'),
+                      ],
+                    ),
+                  ],
+                ),
+              ]),
+            ),
+        ],
       ),
     );
   }
-
-
 }
--- lib/Practice/TrueFalseQuestionPrac.dart
+++ lib/Practice/TrueFalseQuestionPrac.dart
@@ -68,80 +68,110 @@ class _TrueFalseQuestionPracState extends State<TrueFalseQuestionPrac> {
 
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 50,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 50,
+            child: Center(
               child: Text(
                 'Câu hỏi đúng sai',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
-                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35, color: Colors.blueAccent),
               ),
             ),
-            SizedBox(height: 15),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  'Đúng / Sai',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            for (int i = 0; i < subQuestions.length; i++)
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: Row(
-                  children: [
-                    Container(
-                      width: screenWidth * 0.5,
-                      child: Text(
-                        subQuestions[i]['question'],
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: _selectedAnswers[i] != null
-                              ? (_selectedAnswers[i] == _correctAnswers[i]
-                              ? Colors.green
-                              : Colors.red)
-                              : Colors.black,
-                        ),
-                        softWrap: true,
-                        overflow: TextOverflow.visible,
-                        maxLines: 3,
+          ),
+          SizedBox(height: 15),
+          for (int i = 0; i < subQuestions.length; i++)
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 8.0),
+              padding: const EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.grey.shade300),
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.2),
+                    spreadRadius: 3,
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Text(
+                      subQuestions[i]['question'],
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: _selectedAnswers[i] != null
+                            ? (_selectedAnswers[i] == _correctAnswers[i]
+                            ? Colors.green
+                            : Colors.red)
+                            : Colors.black,
                       ),
+                      softWrap: true,
+                      overflow: TextOverflow.visible,
+                      maxLines: 3,
                     ),
-                    Spacer(),
-                    Radio<bool?>(
-                      value: true,
-                      groupValue: _selectedAnswers[i],
-                      onChanged: _isChecked
-                          ? null
-                          : (value) {
-                        setState(() {
-                          _selectedAnswers[i] = value;
-                          _checkAnswers();
-                        });
-                      },
-                    ),
-                    Radio<bool?>(
-                      value: false,
-                      groupValue: _selectedAnswers[i],
-                      onChanged: _isChecked
-                          ? null
-                          : (value) {
-                        setState(() {
-                          _selectedAnswers[i] = value;
-                          _checkAnswers();
-                        });
-                      },
-                    ),
-                  ],
-                ),
+                  ),
+                  SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          SizedBox(
+                            height: 20,
+                            width: 20,
+                            child: Radio<bool?>(
+                              value: true,
+                              groupValue: _selectedAnswers[i],
+                              onChanged: _isChecked
+                                  ? null
+                                  : (value) {
+                                setState(() {
+                                  _selectedAnswers[i] = value;
+                                  _checkAnswers();
+                                });
+                              },
+                            ),
+                          ),
+                          SizedBox(width: 5),
+                          Text('Đúng'),
+                        ],
+                      ),
+                      SizedBox(height: 35,),
+                      Row(
+                        children: [
+                          SizedBox(
+                            height: 20,
+                            width: 20,
+                            child: Radio<bool?>(
+                              value: false,
+                              groupValue: _selectedAnswers[i],
+                              onChanged: _isChecked
+                                  ? null
+                                  : (value) {
+                                setState(() {
+                                  _selectedAnswers[i] = value;
+                                  _checkAnswers();
+                                });
+                              },
+                            ),
+                          ),
+                          SizedBox(width: 5),
+                          Text('Sai'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
               ),
-          ],
-        ),
+            ),
+        ],
       ),
     );
   }
